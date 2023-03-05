d053d71fcbe7fef15332f688745d855397144ea0
Valery Maltsev <lisowsky@yandex.ru>
2023-03-05 20:15:02 +0300

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
-public class Main {
+import java.time.LocalDate;
 
+public class Main {
     public static void checkYear(int year) {
         if(year % 4 == 0 && year % 100 !=0 || year % 400 == 0 ) {
             System.out.println(year + " год является высокосным");
@@ -8,6 +9,30 @@ public class Main {
         }
 
     }
+
+    public static void checkVersionProgramm (int clientOS, int clientDeviceYear){
+        int currentYear = LocalDate.now().getYear();
+        if (clientOS == 0) {
+            if (clientDeviceYear < currentYear) {
+                System.out.println("Установите облегченную версию приложения для IOS по ссылке");
+            }
+            else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        }
+
+
+        else if (clientDeviceYear < currentYear){
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+        else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+
+    }
+
+
 
     public static void main(String[] args) {
         task1();
@@ -19,13 +44,17 @@ public class Main {
     public static void task1() {
         System.out.println("Задача 1");
         // Пишем код для задачи 1
-        int year = 2000;
+        int year = 2021;
         checkYear(year);
     }
 
     public static void task2() {
         System.out.println("Задача 2");
         // Пишем код для задачи 2
+        int clientOS = 0;
+        int clientDeviceYear  = 2022;
+
+        checkVersionProgramm(clientOS, clientDeviceYear);
 
     }
 
